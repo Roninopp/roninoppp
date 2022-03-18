a5e1dbefbf8323e55a6a111354bf1c78d012717e
Roninopp <99870677+Roninopp@users.noreply.github.com>
2022-03-19 01:43:53 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -83,7 +83,7 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 
-SIESTA_IMG = "https://telegra.ph/file/6d3e3ef24f64fb6e35df0.jpg"
+SIESTA_IMG = "https:https://te.legra.ph/file/ea5ec0c634b585c81b264.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
  You can support the project by contacting @saint_foire \
